3c35ecb871d25360cfb257b259f10f9a5ff568bb
Simonas S <simonas.silakauskas@dematic.com>
2020-03-13 16:11:24 +0200

    Changed work directory declaration. Trying to figure out how to fix variables.

--- yearly data extraction.R
+++ yearly data extraction.R
@@ -1,6 +1,9 @@
-setwd('C:\\Users\\Simonas\\Desktop\\Magistrinis\\Magistras GIT') # this line doesn't work on MAC OS
+#setwd('C:\\Users\\Simonas\\Desktop\\Magistrinis\\Magistras GIT')
+# Set working directory to script directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 # converts all factors to numeric type, if levels>3 dummyVars could be considered
-factorsNumeric <- function(d) modifyList(d, lapply(d[, sapply(d, is.factor)], as.numeric))
+# factorsNumeric <- function(d) modifyList(d, lapply(d[, sapply(d, is.factor)], as.numeric))
 
 TIME <- Sys.time()
 
@@ -11,15 +14,21 @@ myData <- myDataRaw
 
 print(Sys.time() - TIME)
 
-#str(myData)
-#head(myData$APRASYMAS_FORMA,20)
-#head(myData$ekch,20)
-#myData$APRASYMAS_FORMA[1]
-#myData$KODAS_STATUSAI[1]
 str(myData)
 
-#myData$DATE <- toString(myData$DATE)
+#!!!!!!!!!!
+# pardavimai_pvm_dekl        : chr  " " " " " " " " ...
+# pvm_dekl_36                : chr  " " " " " " " " ...
+# pirkimai_pvm_dekl          : chr  " " " " " " " " ...
+# laik_tipas_pvm             : chr  "" "" "" "" ...
 
+
+######################## FIX VARIABLES #######################
+# Delete ones with all NA values
+# Numbers: 1,000.00 are read as chars (change to 1000.00?)
+smallData$pardavimai_pvm_dekl <- as.numeric(smallData$pardavimai_pvm_dekl)
+str(smallData)
+summary(myData$pardavimai_pvm_dekl)
 
 ######################## Find duplicate bankcrupt rows ########################
 smallData <- myData[1:200000,]
@@ -29,6 +38,7 @@ write.csv(smallData, file="smallData_200k.csv", row.names = FALSE)
 Bankrupt2018Monthly <- subset(smallData, KODAS_STATUSAI == 5)
 #write.csv(Bankrupt2018Monthly, file="2018_bankrupt.csv", row.names = FALSE)
 nrow(Bankrupt2018Monthly)
+
 # Removing unique values or first of duplicates
 ind_rm <- which(!duplicated(Bankrupt2018Monthly$ekch))
 length(ind_rm)
@@ -36,12 +46,24 @@ length(ind_rm)
 Bankrupt2018MonthlyOnlyDups <- Bankrupt2018Monthly[-ind_rm,]
 #write.csv(Bankrupt2018MonthlyNoDup, file="2018_bankrupt_no_dups.csv", row.names = FALSE)
 
-# Everything left are duplicates
+# Everything left are duplicates with original row indexes
 ind_rm <- as.integer(rownames(Bankrupt2018MonthlyOnlyDups))
 smallData <- smallData[-ind_rm,]
 write.csv(smallData, file="smallData200k_nodups.csv", row.names = FALSE)
 
-##################################
+###############################################################################
+
+########## Variable correleation? - to see which columns are 1:1 () ###########
+#str(smallData)
+#smallData$pirkimai_pvm_dekl[12000:13000]
+#cor(smallData, y=NULL,use="everything", method=c("pearson", "kendall", "spearman"))
+#install.packages("polycor")
+#library("polycor")
+#hetcor(smallData)
+#
+#
+#
+################################################################################
 
 #myData2018Year <- myData[rownames(myData) %like% "DEC",]
 #myData2018Year <- myData[grep("DEC", rownames(myData$DATE)),]
